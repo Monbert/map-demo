74199a37cacf4995e39edda0f70d765044caffa0
Kai Xia <kaixia0815@outlook.com>
2023-05-30 23:09:47 -0400

    simply the function of currentPage at least is 1

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-
     <div style="margin-top: 20px">
       <el-button size="mini" type="danger" @click="handleSelectionDelete()">Delete</el-button>
     </div>
-
     <el-table ref="multipleTable" :data="paginatedData" :row-key="getRowKey" tooltip-effect="dark" style="width: 100%"
       @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" :reserve-selection="true">
@@ -14,13 +12,11 @@
       <el-table-column prop="address" label="Address" show-overflow-tooltip>
       </el-table-column>
     </el-table>
-
     <div class="block">
       <el-pagination layout="prev, pager, next" :total="tableData.length" :page-size="10"
         @current-change="handlePageChange">
       </el-pagination>
     </div>
-
   </div>
 </template>
   
@@ -42,12 +38,11 @@ export default {
       const endIndex = startIndex + 10;
       return this.tableData.slice(startIndex, endIndex);
     },
-
   },
 
   watch: {
     places(newPlaces) {
-      if (typeof newPlaces === 'object' && newPlaces.formatted_address) { // only return gmap allowed search
+      if (newPlaces.formatted_address) { // only return gmap allowed search
         if (!this.tableData.some(item => item.search === newPlaces.name)) { // warning duplicated place
           const searchResult = {
             search: newPlaces.name,
@@ -63,17 +58,16 @@ export default {
       }
       else {
         this.popErrorMessage();
-        // console.log('Cant find the place11');
       }
     },
 
-    currLocation(curloc) {
-      if (!this.tableData.some(item => item.search === 'Your Current Location')) { // warning duplicated place
+    currLocation(currentlocation) {
+      if (!this.tableData.some(item => item.search === 'Your Current Location')) {
         const searchResult = {
           search: 'Your Current Location',
-          address: `lat: ${curloc.lat} lng: ${curloc.lng}`,
-          lat: curloc.lat,
-          lng: curloc.lng
+          address: `lat: ${currentlocation.lat} lng: ${currentlocation.lng}`,
+          lat: currentlocation.lat,
+          lng: currentlocation.lng
         };
         this.tableData.unshift(searchResult); // insert the result at the beginning of the array
         this.$emit('addTableData', this.tableData);
@@ -82,7 +76,6 @@ export default {
   },
 
   methods: {
-
     getRowKey(row) {
       // unique id for row data
       return row.search;
@@ -99,7 +92,6 @@ export default {
     handleSelectionDelete() {
       this.multipleSelection.forEach(row => {
         const index = this.tableData.findIndex(item => item.search === row.search);
-
         if (index != -1) {
           this.tableData.splice(index, 1);
         }
@@ -113,13 +105,8 @@ export default {
 
       // avoid negative numbers when calculating startIndex and endIndex, have to at least is 1
       if (this.currentPage > totalPages) {
-        if (this.currentPage > 1) {
-          this.currentPage--;
-        } else {
-          this.currentPage = 1;
-        }
+        this.currentPage = this.currentPage > 1 ? this.currentPage - 1 : 1;
       }
-
     },
 
     popErrorMessage() {
@@ -133,11 +120,10 @@ export default {
     popWarningMessage() {
       this.$message({
         showClose: true,
-        message: 'Place already exists in table',
+        message: 'This place already exists in table',
         type: 'warning'
       });
     }
-
   }
 }
 </script>
